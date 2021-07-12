3cfd6a4b7ab55fa6cd21fbaa906cd7f85b996fc8
Steve McNally <smcnally@gmail.com>
2021-07-12 13:48:49 -0700

    mb.com and crm.mb updates

--- footer.php
+++ footer.php
@@ -1,21 +1,76 @@
 <?php
 /**
- * The template for displaying all single posts
+ * The template for displaying the footer
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package WordPress
  * @subpackage Twenty_Twenty_One
  * @since 1.0.0
  */
 
-get_header();
+?>
+			</main><!-- #main -->
+		</section><!-- #primary -->
+	</div><!-- #content -->
 
-/* Start the Loop */
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'content-page' );
+	<footer id="colophon" class="site-footer" role="contentinfo">
 
-endwhile; // End of the loop.
+		<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
 
-get_footer();
+		<div class="site-info">
+		<!--
+			<div class="site-name">
+				<?php if ( has_custom_logo() ) : ?>
+					<div class="site-logo"><?php the_custom_logo(); ?></div>
+					<?php
+					else :
+						$blog_info = get_bloginfo( 'name' );
+						if ( ! empty( $blog_info ) ) :
+							if ( is_front_page() && ! is_paged() ) :
+								?>
+								<?php bloginfo( 'name' ); ?>
+							<?php else : ?>
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+								<?php
+								endif;
+							endif;
+						endif;
+					?>
+			</div>
+					-->
+			<!-- .site-name -->
+
+			<div class="copyright">
+				<?php
+				/* translators: 1: Copyright date format, see https://www.php.net/manual/datetime.format.php, 2: Site name */
+				printf(
+					/* Translators: %1$s: Copyright date. %2$s: Site name. */
+					esc_html__( '&copy; %1$s %2$s', 'twentytwentyone' ),
+					esc_html( date_i18n( _x( 'Y', 'copyright date format', 'twentytwentyone' ) ) ),
+					esc_html( get_bloginfo( 'name' ) . '.' )
+				);
+
+				/**
+				 * WIP: Remove or add spacing?
+				if ( function_exists( 'the_privacy_policy_link' ) ) {
+					the_privacy_policy_link();
+				}
+				 */
+				?>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwentyone' ) ); ?>"  class="imprint">
+				<?php /* translators: %s: WordPress. */ printf( esc_html__( 'Powered by %s.', 'twentytwentyone' ), 'WordPress 2021' ); ?>
+				</a>
+			</div><!-- .copyright -->
+
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- page.php
+++ page.php
@@ -14,12 +14,9 @@ get_header();
 /* Start the Loop */
 while ( have_posts() ) :
 	the_post();
-	get_template_part( 'template-parts/content/content-page' );
+	get_template_part( 'content-page' );
 
-	// If comments are open or we have at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
 endwhile; // End of the loop.
 
-get_footer();
+/* Mostly ensuring these child files are In Effect and WaD */
+/* get_footer(); */
